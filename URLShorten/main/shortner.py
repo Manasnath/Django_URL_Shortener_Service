56ca8816daa51b2bdd7074897ecfde601be5e6ff
Manasnath <manasnath437@gmail.com>
2022-06-20 14:43:22 +0530

    first commit

---
+++ URLShorten/main/shortner.py
@@ -0,0 +1,14 @@
+from lib2to3.pgen2 import token
+import random
+import string
+
+class shortner:
+    token_size = 5
+
+    def __init__(self, token_size=None):
+        self.token_size = token_size if token_size is not None else 5
+
+    def issue_token(self):
+        letters = string.ascii_letters
+        return ''.join(random.choice(letters) for i in range(self.token_size))
+        
